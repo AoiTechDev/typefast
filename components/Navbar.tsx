b370758df069d80d048f062b1f01f1aeb69c97d2
Pawel <pawel.bornikowski@gmail.com>
2026-08-21 01:13:48 +0200

    add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="border-b-5 border-b-ink p-4">
+      <div className="text-[28px] font-extrabold space-x-2">
+        <span>TYPE</span>
+        <span className="border-3 py-[0.5px] px-2 bg-lime ">FAST</span>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
